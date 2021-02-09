3056a8e752ef35a43010f28d3a3dcaabb85098e1
aurelia marchand <aurelia.marchand.95@gmail.com>
2021-02-09 12:05:50 +0100

    integration test parkingspot

---
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingSpotDaoIT.java
@@ -0,0 +1,83 @@
+package com.parkit.parkingsystem.integration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.parkit.parkingsystem.dao.ParkingSpotDao;
+import com.parkit.parkingsystem.dao.TicketDao;
+import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
+import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.Ticket;
+import com.parkit.parkingsystem.service.ParkingService;
+import com.parkit.parkingsystem.util.InputReaderUtil;
+
+@ExtendWith(MockitoExtension.class)
+class ParkingSpotDaoIT {
+
+  private static ParkingSpotDao parkingSpotDAO;
+
+  private static ParkingService parkingService;
+
+  @Mock
+  private static InputReaderUtil inputReaderUtil;
+
+  private static TicketDao ticketDAO;
+
+  private static DataBaseTestConfig dataBaseTestConfig = new DataBaseTestConfig();
+
+  private static DataBasePrepareService dataBasePrepareService;
+
+  @BeforeAll
+  private static void setUp() throws Exception {
+    parkingSpotDAO = new ParkingSpotDao();
+    parkingSpotDAO.dataBaseConfig = dataBaseTestConfig;
+    ticketDAO = new TicketDao();
+    ticketDAO.dataBaseConfig = dataBaseTestConfig;
+
+  }
+
+  @BeforeEach
+  private void setUpPerTest() throws Exception {
+    dataBasePrepareService = new DataBasePrepareService();
+  }
+
+  @AfterAll
+  private static void tearDown() {
+    dataBasePrepareService.clearDataBaseEntries();
+  }
+
+  @Test
+  public void testGetNextAvailableSlotCarFirstIsOne() throws Exception {
+    when(inputReaderUtil.readSelection()).thenReturn(1);
+    when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("CAR01");
+
+    parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+
+    parkingService.processIncomingVehicle();
+    Ticket ticket = ticketDAO.getCompletTicket("CAR01");
+
+    assertEquals(1, ticket.getParkingSpot().getId());
+  }
+
+  @Test
+  public void testGetNextAvailableSlotBikeFirstIsFour() throws Exception {
+    when(inputReaderUtil.readSelection()).thenReturn(2);
+    when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("BIKE01");
+
+    parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+
+    parkingService.processIncomingVehicle();
+    Ticket ticket = ticketDAO.getCompletTicket("BIKE01");
+    assertEquals(4, ticket.getParkingSpot().getId());
+
+  }
+
+}
